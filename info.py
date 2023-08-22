270937afa7b3f68ab884b375c3ef9be84ed0568d
Goopalamoorthy <142672172+Goopalamoorthy@users.noreply.github.com>
2023-08-22 09:31:48 +0530

    Update info.py

--- info.py
+++ info.py
@@ -36,8 +36,8 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "kumaran1")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', ))
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', '')
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001832202316'))
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'SMD_Owner')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "False")), False)
 IMDB = is_enabled((environ.get('IMDB', "False")), False)
 SINGLE_BUTTON = is_enabled((environ.get('SINGLE_BUTTON', "True")), True)
